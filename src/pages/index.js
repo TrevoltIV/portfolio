17cc4deeb10d3a0cb1ade6d85b2ddf163e8bc964
TrevoltIV <kgk1999@gmail.com>
2023-05-03 12:44:15 -0400

    more escapes ugh

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.container}>
-        <Header title="My Portfolio" />
+        <Header title="Karsten Koerner" />
         <div className={styles.content}>
           <Welcome title="Welcome to my portfolio!" />
           <List />
