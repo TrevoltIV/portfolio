9c1169f5a109b9e7fcc29a03dda2d9ed15b9afa8
TrevoltIV <kgk1999@gmail.com>
2023-05-06 11:56:13 -0400

    edit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,15 +12,9 @@ export default function Welcome(props) {
                 </h2>
             </div>
             <div className={styles.content}>
-            {/* <p className={styles.text}>
-                    {`My name is Karsten Koerner. I am a web developer based out of Atlanta, GA. I have been programming for about 2 years now. I never expected to get sucked in to web development, it just sort of happened. I had an idea for a website I
-                    wanted to make, so I started learning HTML, thinking it would be a weekend task (lol), but I ended up going on a 6 month long journey, at the end of which I had good knowledge of PHP, JavaScript, and of course HTML and CSS.
-                    But then I discovered React, and my whole world changed. I am now fluent in Next.js, SQL/NoSQL databases, Chrome extension development, and more!`}
+                <p>
+                    I have created all the websites below without the use of any No-Code software such as Wordpress. They are solely developed using Next.js and other libraries/frameworks.
                 </p>
-                <p className={styles.text}>
-                    {`This page is a simple portfolio, as you can see it is not particularly creative. Below is a list of example websites I have built, so you can see some designs I can build with ease. Keep in mind that these are basic templates, and I am not
-                    a designer, so if you have a web designer, I am sure we will make a great team.`}
-                </p> */}
             </div>
         </div>
     )
